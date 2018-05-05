2144cb93562a7964a579439902cc7090e25ff9a8
jverbosk <jverbosk@gmail.com>
2018-05-04 23:18:38 -0400

    completed lesson 10

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -19,6 +19,7 @@ namespace Engine
             Gold = gold;
             ExperiencePoints = experiencePoints;
             Level = level;
+
             Inventory = new List<InventoryItem>();
             Quests = new List<PlayerQuest>();
         }
